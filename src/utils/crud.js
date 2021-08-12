6c10e5a3d232c197c268f37ff5666b46b90f7014
Tan Theng Lee <thengleetan@gmail.com>
2021-08-12 17:43:48 +0800

    Create utils CRUD resolvers

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,59 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const result = await model.findById(req.params.id)
 
-export const getMany = model => async (req, res) => {}
+  if (result !== null) {
+    res.status(200).json({ data: result })
+  } else {
+    res.status(400).end()
+  }
+}
 
-export const createOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const result = await model.find({ createdBy: req.user._id }).exec()
+  if (result !== null) {
+    res.status(200).json({ data: result })
+  } else {
+    res.status(400).end()
+  }
+}
 
-export const updateOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  model.create(
+    {
+      name: req.body.name,
+      createdBy: req.user._id
+    },
+    (err, doc) => {
+      if (err) {
+        res.status(400).end()
+      } else {
+        res.status(201).json({ data: doc })
+      }
+    }
+  )
+}
 
-export const removeOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  const doc = await model
+    .findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    .exec()
+  if (doc !== null) {
+    res.status(200).json({ data: doc })
+  } else {
+    res.status(400).end()
+  }
+}
+
+export const removeOne = model => async (req, res) => {
+  const doc = await model.findByIdAndDelete(req.params.id)
+  if (doc !== null) {
+    res.status(200).json({ data: doc })
+  } else {
+    res.status(400).end()
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
